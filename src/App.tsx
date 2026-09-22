5596fd8ce0889614ac8a601994435faabfcd7283
inori-3333 <inori333@163.com>
2026-09-22 08:31:53 +0800

    Format sidebar attribution

--- src/App.tsx
+++ src/App.tsx
@@ -137,8 +137,7 @@ export default function App() {
           <div className="local-note">
             <span className="live-dot" />
             <div>
-              启航华电 15 组
-              <small>青岛公司 张宇辰</small>
+              启航华电 15 组<small>青岛公司 张宇辰</small>
             </div>
           </div>
         </div>
